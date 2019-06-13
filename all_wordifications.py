e9fc9a0e2e3b0a06fea69a73a9ccc46caf9bffdc
Michelle George <mgeorge40@gatech.edu>
2019-06-13 18:40:35 -0400

    Changed English dictionary and made search for valid words faster

--- all_wordifications.py
+++ all_wordifications.py
@@ -13,19 +13,19 @@ def all_wordifications(number):
     if number[0:6] != '1-800-' and (len(number) != 13 or len(number) != 14):
         raise Exception('The input should be a phone number starting with 1-800- followed by 7 more digits. The inputted phone number was {}'.format(number))
     groups = number.split('-')  # separate groups of numbers, removing dashes
-    # FOR NOW assume number was inputted correctly with 1-800- at start
     numbers = ''.join(groups[2:]) # join all groups of numbers that were separated by dashes
     if not numbers.isdigit() or '0' in numbers or '1' in numbers:
         raise Exception('The input should be a phone number starting with 1-800- followed by 7 more integers between 2 and 9, containing no letters. The inputted phone number was {}'.format(number))
     # Create an output file to store all your word/number combinations in
     f = open("wordifications.txt", "w+")
     length = len(numbers)
+    english_words = set(words.words())
     # try the longest words first, then step down your length (l) if no words are found
-    for l in range(length, 0, -1):
+    for l in range(length, 1, -1):
         # increment your starting position (s)
         for s in range(0, length-l+1, 1):
             phone_list = []
-            wordlist = getwordlist(numbers[s:s+l], '', 0, [])
+            wordlist = getwordlist(numbers[s:s+l], '', 0, [], english_words)
             # use getwordlist function, will return a list of all possible words at this start position and length
             if wordlist != None:
                 for word in wordlist:
@@ -35,14 +35,14 @@ def all_wordifications(number):
     f.close()
     return
 
-def getwordlist(numbers, output, count, phone_list):
+def getwordlist(numbers, output, count, phone_list, english_words):
     letters = dictionary[numbers[count]]
     for n in letters: # cycles through each possible letter for the corresponding number
         output = output + n # add that letter to your word
         if count+1 != len(numbers): # if we haven't reached the end of the numbers, loop again
-            phone = getwordlist(numbers, output, count+1, phone_list)
+            phone = getwordlist(numbers, output, count+1, phone_list, english_words)
         else: # check to see if our now full string is a word
-            if output.lower() in words.words():
+            if output.lower() in english_words:
                 phone = output
                 phone_list.append(phone) # if it is a word, add it to our output list
         output = output[:-1] # remove the last letter so in the next loop we can replace it
--- number_to_words.py
+++ number_to_words.py
@@ -1,8 +1,8 @@
 # Function number_to_words() will convert a phone number into an English word
-# Currently finds the longest English word that corresponds to the phone number or a subset of the phone number
+# Finds the longest English word that corresponds to the phone number or a subset of the phone number
 # author Michelle George, mgeorge40@gatech.edu
 
-from nltk.corpus import wordnet
+from nltk.corpus import words
 
 dictionary = {'2':'ABC', '3':'DEF', '4':'GHI', '5':'JKL', '6':'MNO', '7':'PQRS', '8':'TUV', '9':'WXYZ'}
 
@@ -13,33 +13,33 @@ def number_to_words(number):
     if number[0:6] != '1-800-' and (len(number) != 13 or len(number) != 14):
         raise Exception('The input should be a phone number starting with 1-800- followed by 7 more digits. The inputted phone number was {}'.format(number))
     groups = number.split('-')  # separate groups of numbers, removing dashes
-    # FOR NOW assume number was inputted correctly with 1-800- at start
     numbers = ''.join(groups[2:]) # join all groups of numbers that were separated by dashes
     if not numbers.isdigit() or '0' in numbers or '1' in numbers:
         raise Exception('The input should be a phone number starting with 1-800- followed by 7 more integers between 2 and 9, containing no letters. The inputted phone number was {}'.format(number))
     length = len(numbers)
+    wordlist = set(words.words())
     # try the longest words first, then step down your length (l) if no words are found
     for l in range(length, 0, -1):
         # increment your starting position (s)
         for s in range(0, length-l+1, 1):
-            word = getword(numbers[s:s+l], '', 0)
+            word = getword(numbers[s:s+l], '', 0, wordlist)
             # use getword function, if it returns None then no real English word was found at this start position and length
             if word != None:
                 # if it is a word, create a phone number with it (add back the 1-800- and any unused numbers)
                 phone = create_phonenumber(word, numbers, s, l)
                 return phone
 
-def getword(numbers, output, count):
+def getword(numbers, output, count, wordlist):
     letters = dictionary[numbers[count]]
     for n in letters: # cycles through each possible letter for the corresponding number
         output = output + n # add that letter to your word
         if count+1 != len(numbers): # if we haven't reached the end of the numbers, loop again
-            phone = getword(numbers, output, count+1)
+            phone = getword(numbers, output, count+1, wordlist)
             # once the lowest level recursion returns, we want to save this string which is now our phone number
             if phone != None:
                 return phone
         else: # check to see if our now full string is a word
-            if wordnet.synsets(output.lower()):
+            if output.lower() in wordlist:
                 phone = output
                 return phone
         output = output[:-1] # remove the last letter so in the next loop we can replace it
